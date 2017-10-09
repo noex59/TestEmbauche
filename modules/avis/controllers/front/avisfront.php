96344623eb79f955a0476f259d61309d8d5d6ad1
François <fvantours@gmail.com>
2017-10-09 13:52:34 +0200

    Projet 'fini' - Admin ok et avis aleat ok

--- modules/avis/controllers/front/avisfront.php
+++ modules/avis/controllers/front/avisfront.php
@@ -1,18 +1,35 @@
 <?php
     class avisAvisfrontModuleFrontController extends ModuleFrontController
     {
-        public function getAvis()
+        /*public function IdAleat()
         {
-            $query = "SELECT titre, contenu, dateAjout, dateUpdate FROM ps_avis";
+            $query = "SELECT id FROM ps_avis";
+            $res = Db::getInstance()->ExecuteS($query);
+            $allId = array_rand($res, 1);
+            return $allId;
+        }*/
+
+        public function test()
+        {
+            $query = "SELECT id FROM ps_avis ORDER BY RAND() LIMIT 1";
+            $res = Db::getInstance()->ExecuteS($query);
+            var_dump($res[0]['id']);
+            return $res[0]['id'];
+        }
+
+
+        public function getAvisAleat()
+        {
+            $query = "SELECT titre, contenu, dateAjout, dateUpdate FROM ps_avis WHERE id = ".$this->test();
             $res = Db::getInstance()->ExecuteS($query);
             return $res;
         }
 
         public function initContent()
         {
-            parent::initContent();        
+            parent::initContent();
             $this->context->smarty->assign(array(
-                'hello' => 'Hello World'
+                'contenuAvis' => $this->getAvisAleat()
             ));
             $this->setTemplate('module:avis/views/templates/front/avis.tpl');
         }
--- modules/avis/views/templates/front/avis.tpl
+++ modules/avis/views/templates/front/avis.tpl
@@ -4,4 +4,21 @@
 <h3>Votre avis compte</h3>
 <h6>En voici un exemple au hasard :</h6>
 
+<table style="width:100%;border: 1px solid black;">
+  <tr style="border: 1px solid black;">
+    <th>Titre</th>
+    <th>Contenu</th> 
+    <th>Date d'ajout</th>
+    <th>Date de modif</th>
+  </tr>
+{foreach from=$contenuAvis item=row}
+	  <tr style="border: 1px solid black;">
+	    <td>{$row['titre']}</td>
+	    <td>{$row['contenu']}</td> 
+	    <td>{$row['dateAjout']}</td>
+	    <td>{$row['dateUpdate']}</td>
+	  </tr>
+  {/foreach}
+</table>
+
 {/block}
